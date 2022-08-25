f16d29b9f55d628356b4f55f296a3afd64b33998
Luis Sebastian Delgado Bruno <admafan2@gmail.com>
2022-08-25 19:05:45 +0200

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+  # has_many :restaurants, dependent: :destroy
+  # has_many :reviews, dependent: :destroy
 end
